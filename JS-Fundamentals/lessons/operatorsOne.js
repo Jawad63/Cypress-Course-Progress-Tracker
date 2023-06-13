5912b84053f227397b007ce8aa896aab3ad62b71
jawad qureshi <jawadqureshi111@gmail.com>
2023-06-13 21:45:20 +0200

    relational and equality operators covvered

---
+++ JS-Fundamentals/lessons/operatorsOne.js
@@ -0,0 +1,16 @@
+// Relational or comparison operators:
+
+/*
+> : more than
+< : less than
+>= : more than or equal to
+<= : less than or equal to
+*/
+
+var result = 5 <= 5;
+console.log(result);
+
+// equality operators:
+var x = 1;
+console.log(x == '1'); // loose comparison
+console.log(x === '1'); // strict comparison
